aa55c9a82a6c4907f783d83558e91fc62510f829
Denis Mayorov <denismaiorov2009@yandex.ru>
2022-10-13 12:51:50 +0700

    Dom zadanie3

---
+++ main.js
@@ -0,0 +1,14 @@
+let numbers = [2,5,13,7,6,4];
+size = 6;
+sum = 0;
+avg = 0;
+index = 0;
+
+if (index < size) {
+    sum = sum + numbers[index]
+    index = index + 1
+}else {
+    (avg = sum / size)
+}
+print (avg) 
+
